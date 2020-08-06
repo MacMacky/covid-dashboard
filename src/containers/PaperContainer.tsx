3d3ca3ae7b38efd2b5ce77dd6c0b544e1b3a7d1d
Mark <mark@dnamicro.com>
2020-08-07 07:39:33 +0800

    Modify add new prop 'justify'

--- src/containers/PaperContainer.tsx
+++ src/containers/PaperContainer.tsx
@@ -1,27 +1,28 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles'
-import { Grid, Paper } from '@material-ui/core'
+import { Grid, Paper, GridContentAlignment } from '@material-ui/core'
 
 const paperContainerStyles = makeStyles({
-  root: {
-    margin: 30,
-    width: 'auto',
-    height: 'auto',
-    padding: 25
-  }
+	root: {
+		margin: 10,
+		width: 'auto',
+		height: 'auto',
+		padding: 25
+	}
 })
 
-const PaperContainer = ({ children }: { children: React.ReactNode }) => {
-  const styles = paperContainerStyles()
-  return (
-    <Grid container component={Paper}
-      elevation={4}
-      className={styles.root}
-      children={children}
-    />
-  )
+const PaperContainer = ({ children, justify = 'flex-start' }: { children: React.ReactNode, justify?: GridContentAlignment | any }) => {
+	const styles = paperContainerStyles()
+	return (
+		<Grid container component={Paper}
+			elevation={4}
+			justify={justify}
+			className={styles.root}
+			children={children}
+		/>
+	)
 }
 
 export {
-  PaperContainer
+	PaperContainer
 }

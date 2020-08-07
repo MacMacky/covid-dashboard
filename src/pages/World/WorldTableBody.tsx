eafc0b2f5d19bfed31445eb6a8b3b47220c8f740
Mark <mark@dnamicro.com>
2020-08-07 11:52:49 +0800

    Modify use 'useHistory' and pass 'history' as props

--- src/pages/World/WorldTableBody.tsx
+++ src/pages/World/WorldTableBody.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Align, RowData } from '../../helpers/types'
+import WorldTableRow from './WorldTableRow'
 import { TableBody } from '@material-ui/core'
-import WorldTableRow from './WorldTableRow';
+import { useHistory } from 'react-router-dom'
+import { Align, RowData } from '../../helpers/types'
 
 const WorldTableBody = ({ data, align = "center" }: { data: RowData[], align?: Align }) => {
+	const history = useHistory()
+
 	return (
 		<TableBody>
 			{data.map(d =>
-				<WorldTableRow key={d.country} {...d} align={align} />
+				<WorldTableRow key={d.country} {...d} align={align} history={history} />
 			)}
 		</TableBody>
 	)
+
 }
 
 export default WorldTableBody

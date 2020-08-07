ea9c9af924ab52347ed33ed47ae0ee48de5d70dc
Mark <mark@dnamicro.com>
2020-08-07 09:47:32 +0800

    Modify 'World' page use 'reducer' instead of 'useStates'

--- src/pages/World/index.tsx
+++ src/pages/World/index.tsx
@@ -1,67 +1,54 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useReducer, useCallback } from 'react'
 import WorldTableHead from './WorldTableHead'
 import WorldTableBody from './WorldTableBody'
 import TableLoadingRow from '../../components/TableLoadingRow'
 import { useFetch } from '../../helpers/hooks'
 import { SortContext } from '../../helpers/contexts'
 import { PaperContainer } from '../../containers/PaperContainer'
-import { log, ModifyResponseCallback } from '../../helpers'
-import { TableContainer, Paper, Table } from '@material-ui/core'
-import { RowData } from '../../helpers/types'
-
-interface SortOption {
-	sortBy: keyof RowData
-	orderBy?: 'asc' | 'desc'
-	type?: 'string' | 'number'
-}
-
-const sortComparison = ({ type, sortBy, orderBy }: SortOption) => (a: any, b: any) => {
-	if (type === 'string') {
-		if (orderBy === 'asc') {
-			return a[sortBy].localeCompare(b[sortBy])
-		} else {
-			return b[sortBy].localeCompare(a[sortBy])
-		}
-	} else {
-		if (orderBy === 'asc') {
-			return a[sortBy] - b[sortBy]
-		} else {
-			return b[sortBy] - a[sortBy]
-		}
-	}
-}
-
-
-const sortCallback = (oldData: RowData[], options: SortOption) => {
-	const newData = oldData.slice().sort(sortComparison(options))
-	return newData
-}
+import { SortOption } from '../../helpers/types'
+import { ModifyResponseCallback } from '../../helpers'
+import { TableContainer, Paper, Table, TextField } from '@material-ui/core'
+import reducer, { SET_CASES, SORT_CASES, UPDATE_SEARCH, initialState } from '../../helpers/reducers/world'
 
 const World = () => {
 	const [error, loading, response] = useFetch('https://coronavirus-19-api.herokuapp.com/countries', ModifyResponseCallback)
-	const [data, setSortedData] = useState<RowData[] | null>(null)
-	const [options, setOptions] = useState<SortOption>({ orderBy: 'desc', sortBy: 'country' })
+	const [{ filteredAndSortedCases, options, searchTerm }, dispatch] = useReducer(reducer, initialState)
 
 	useEffect(() => {
-		setSortedData(response)
+		const timeout = setTimeout(() => {
+			dispatch({ type: UPDATE_SEARCH, payload: searchTerm })
+		}, 300)
+		return () => clearTimeout(timeout)
+	}, [searchTerm])
+
+	useEffect(() => {
+		dispatch({ type: SET_CASES, payload: response })
 	}, [loading, response])
 
-	const sortData = ({ sortBy, type }: SortOption) => {
-		const order = options.orderBy === 'asc' ? 'desc' : 'asc'
-		setOptions({ sortBy, orderBy: order, type })
-		setSortedData(state => sortCallback(state as RowData[],
-			{ orderBy: order, sortBy, type }))
-	}
+	const sortData = useCallback(({ sortBy, type }: SortOption) => {
+		dispatch({
+			type: SORT_CASES,
+			payload: {
+				sortBy, type
+			}
+		})
+	}, [dispatch])
 
-	return <SortContext.Provider value={{ data, sortData }}>
-		<PaperContainer>
+	const handleSearchChange = useCallback(({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
+		dispatch({ type: UPDATE_SEARCH, payload: value })
+	}, [dispatch])
+
+	return <SortContext.Provider value={{ data: filteredAndSortedCases, sortData }}>
+		<PaperContainer justify="center">
+			<TextField variant="outlined" label="Search..." value={searchTerm} onChange={handleSearchChange} />
+
 			<TableContainer component={Paper}>
 				<Table>
 					<WorldTableHead opts={options} />
 					{
 						loading ?
 							<TableLoadingRow />
-							: <WorldTableBody data={data || []} />
+							: <WorldTableBody data={filteredAndSortedCases} />
 					}
 				</Table>
 			</TableContainer>

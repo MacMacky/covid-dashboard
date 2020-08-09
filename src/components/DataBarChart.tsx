2bde790b254f33ed6d381d5d6eeceb32499c67ef
Mark <mark@dnamicro.com>
2020-08-09 10:21:03 +0800

    Modify component functionality use 'CountryToolTip' component

--- src/components/DataBarChart.tsx
+++ src/components/DataBarChart.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import CountryToolTip from './CountryToolTip'
 import { Skeleton } from '@material-ui/lab'
+import { ChartProps } from '../helpers/types'
 import { ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts'
 
-const DataBarChart = ({ loading, data, color, dataKey }: { loading: boolean, data: any[], color: string, dataKey: string }) => {
+
+const DataBarChart = ({ loading, data, color, dataKey }: ChartProps) => {
 	return (
 		<ResponsiveContainer width={1000} height={250}>
 			{loading ? <Skeleton height={250} variant="rect" /> :
-				<BarChart data={data}>
+				<BarChart data={data || []}>
 					<CartesianGrid strokeDasharray="3 3" />
 					<XAxis dataKey="date" />
 					<YAxis width={100} />
-					<Tooltip />
+					<Tooltip content={CountryToolTip} />
 					<Legend verticalAlign="top" />
 					<Bar dataKey={dataKey} fill={color} />
 				</BarChart>

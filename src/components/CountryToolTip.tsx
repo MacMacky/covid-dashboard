f400909f79cc3007b84ae580ef48c8bd0ea315a3
Mark <mark@dnamicro.com>
2020-08-09 10:12:16 +0800

    Add new helper component 'CountryToolTip'

---
+++ src/components/CountryToolTip.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { capitalize } from '../helpers'
+import { Paper, Typography } from '@material-ui/core'
+
+const CountryToolTip = ({ payload, label, active }: { payload: any, label: string, active: boolean }) => {
+	if (active) {
+		const { dataKey, value, color } = payload[0]
+		const { code, country } = payload[0].payload
+		return (
+			<Paper style={{ padding: 15, textAlign: 'center' }}>
+				<Typography variant="h5" children={country} />
+				<img src={`https://flagcdn.com/w40/${code}.png`} />
+				<Typography variant="h6" children={`${capitalize(dataKey)}: ${value}`} style={{ color }} />
+				<Typography variant="body1" children={label} />
+			</Paper>
+		);
+	}
+
+	return null;
+}
+
+export default CountryToolTip

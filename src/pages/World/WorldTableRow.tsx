42ee83ccd80a26315bfdb830c7b254ab9cdbfd0b
Mark Abeto <mac21macky@gmailcom>
2020-08-10 17:17:29 +0800

    Fix issue 'pointer' cursor not working and remove redundant passed state data

--- src/pages/World/WorldTableRow.tsx
+++ src/pages/World/WorldTableRow.tsx
@@ -5,32 +5,32 @@ import { Align, RowData } from '../../helpers/types'
 import { TableCell, TableRow } from '@material-ui/core'
 
 interface WorldTableRowData extends RowData {
-	align?: Align,
-	history: History
+  align?: Align,
+  history: History
 }
 
 const WorldTableRow = memo(({ active, align, cases, todayCases, deaths, recovered, critical, code, country, history }: WorldTableRowData) => {
-	return (
-		<TableRow hover
-			style={{ cursor: 'hover' }}
-			onClick={() => {
-				const _country = (country.includes(' ') ? country.split(' ').join('-') : country).toLowerCase()
-				history.push(`/world/${_country}`, { country, code: code?.toLowerCase(), totalCases: cases, totalDeaths: deaths, totalRecovered: recovered })
-			}}>
-			<TableCell align={align}>
-				{code ? <img src={`https://flagcdn.com/w40/${code.toLowerCase()}.png`} /> :
-					null}
-				<br />
-				{country}
-			</TableCell>
-			<TableCell align={align}>{formatNumber(cases)}</TableCell>
-			<TableCell align={align}>{formatNumber(todayCases)}</TableCell>
-			<TableCell align={align}>{formatNumber(deaths)}</TableCell>
-			<TableCell align={align}>{formatNumber(recovered)}</TableCell>
-			<TableCell align={align}>{formatNumber(active)}</TableCell>
-			<TableCell align={align}>{formatNumber(critical)}</TableCell>
-		</TableRow>
-	)
+  return (
+    <TableRow hover
+      style={{ cursor: 'pointer' }}
+      onClick={() => {
+        const _country = (country.includes(' ') ? country.split(' ').join('-') : country).toLowerCase()
+        history.push(`/world/${_country}`, { country, code: code?.toLowerCase() })
+      }}>
+      <TableCell align={align}>
+        {code ? <img src={`https://flagcdn.com/w40/${code.toLowerCase()}.png`} /> :
+          null}
+        <br />
+        {country}
+      </TableCell>
+      <TableCell align={align}>{formatNumber(cases)}</TableCell>
+      <TableCell align={align}>{formatNumber(todayCases)}</TableCell>
+      <TableCell align={align}>{formatNumber(deaths)}</TableCell>
+      <TableCell align={align}>{formatNumber(recovered)}</TableCell>
+      <TableCell align={align}>{formatNumber(active)}</TableCell>
+      <TableCell align={align}>{formatNumber(critical)}</TableCell>
+    </TableRow>
+  )
 })
 
 

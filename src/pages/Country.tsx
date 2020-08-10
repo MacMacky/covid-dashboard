1a58cbc5700119de64753e36a00a5c454f5128ea
Mark <mark@dnamicro.com>
2020-08-10 12:49:12 +0800

    Modify component logic

--- src/pages/Country.tsx
+++ src/pages/Country.tsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
 import '../App.css'
 import renderChart from '../components/renderChart'
+import LabelWithIcon from '../components/LabelWithIcon';
 import { PaperContainer } from '../containers/PaperContainer'
 import { RouteComponentProps } from 'react-router-dom'
 import { modifyResponseCountryCB } from '../helpers'
 import { useFetch, useChangeDocumentTitle } from '../helpers/hooks';
 import { CountryData, Chart, CountryRouteProps } from '../helpers/types'
-import { Grid, Select, MenuItem, FormControl, InputLabel } from '@material-ui/core'
+import { Grid, Select, MenuItem, FormControl, InputLabel, colors } from '@material-ui/core'
+import { GroupAdd, PersonAddDisabled, PeopleAlt, Language, Report } from '@material-ui/icons'
 
 
 const Country = ({ location: { state } }: RouteComponentProps<{}, {}, CountryRouteProps>) => {
 	const [error, loading, response] = useFetch<CountryData>('https://pomber.github.io/covid19/timeseries.json',
-		modifyResponseCountryCB(state), {})
+		modifyResponseCountryCB({ country: state.country, code: state.code }), {})
 	const [chart, setChart] = useState<Chart>('bar')
 
 	const handleChartChange = (e: any): void => {
@@ -21,7 +23,12 @@ const Country = ({ location: { state } }: RouteComponentProps<{}, {}, CountryRou
 	useChangeDocumentTitle()
 
 	return (
-		<PaperContainer justify="center">
+		<PaperContainer>
+			<Grid container justify="center" style={{ marginBottom: 50 }}>
+				<LabelWithIcon label="Total Cases" loading={loading} value={state.totalCases} color={colors.yellow[700]} Icon={PeopleAlt} />
+				<LabelWithIcon label="Total Deaths" loading={loading} value={state.totalDeaths} color={colors.red[400]} Icon={PersonAddDisabled} />
+				<LabelWithIcon label="Total Recovered" loading={loading} value={state.totalRecovered} color={colors.green[500]} Icon={GroupAdd} />
+			</Grid>
 			<Grid item>
 				<Grid container justify="center">
 					<FormControl variant="outlined" style={{ minWidth: 250, marginBottom: 15 }}>
@@ -33,7 +40,9 @@ const Country = ({ location: { state } }: RouteComponentProps<{}, {}, CountryRou
 						</Select>
 					</FormControl>
 				</Grid>
-				{renderChart(chart, loading, response)}
+				<Grid container justify="center">
+					{renderChart(chart, loading, response)}
+				</Grid>
 			</Grid>
 		</PaperContainer  >
 	)

4ea34606478aec1495c8d1458299330a8a1e095e
Mark Abeto <mac21macky@gmailcom>
2020-08-10 14:33:06 +0800

    Modify use config url

--- src/pages/World/index.tsx
+++ src/pages/World/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer, useCallback } from 'react'
 import WorldTableHead from './WorldTableHead'
 import WorldTableBody from './WorldTableBody'
 import TableLoadingRow from '../../components/TableLoadingRow'
+import { MAIN_API } from '../../helpers/config'
 import { SortContext } from '../../helpers/contexts'
 import { SortOption } from '../../helpers/types'
 import { PaperContainer } from '../../containers/PaperContainer'
@@ -11,60 +12,60 @@ import { TableContainer, Paper, Table, TextField } from '@material-ui/core'
 import reducer, { SET_CASES, SORT_CASES, UPDATE_SEARCH, initialState } from '../../helpers/reducers/world'
 
 const World = () => {
-	const [error, loading, response] = useFetch('https://coronavirus-19-api.herokuapp.com/countries', ModifyResponseCallback)
-	const [{ filteredAndSortedCases, options, searchTerm }, dispatch] = useReducer(reducer, initialState)
+  const [error, loading, response] = useFetch(MAIN_API, ModifyResponseCallback)
+  const [{ filteredAndSortedCases, options, searchTerm }, dispatch] = useReducer(reducer, initialState)
 
-	useChangeDocumentTitle()
+  useChangeDocumentTitle()
 
-	useEffect(() => {
-		const timeout = setTimeout(() => {
-			dispatch({ type: UPDATE_SEARCH, payload: searchTerm })
-		}, 300)
-		return () => clearTimeout(timeout)
-	}, [searchTerm])
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      dispatch({ type: UPDATE_SEARCH, payload: searchTerm })
+    }, 300)
+    return () => clearTimeout(timeout)
+  }, [searchTerm])
 
-	useEffect(() => {
-		dispatch({ type: SET_CASES, payload: response })
-	}, [loading, response])
+  useEffect(() => {
+    dispatch({ type: SET_CASES, payload: response })
+  }, [loading, response])
 
-	const sortData = useCallback(({ sortBy, type }: SortOption) => {
-		dispatch({
-			type: SORT_CASES,
-			payload: {
-				sortBy, type
-			}
-		})
-	}, [dispatch])
+  const sortData = useCallback(({ sortBy, type }: SortOption) => {
+    dispatch({
+      type: SORT_CASES,
+      payload: {
+        sortBy, type
+      }
+    })
+  }, [dispatch])
 
-	const handleSearchChange = useCallback(({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
-		dispatch({ type: UPDATE_SEARCH, payload: value })
-	}, [dispatch])
+  const handleSearchChange = useCallback(({ target: { value } }: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({ type: UPDATE_SEARCH, payload: value })
+  }, [dispatch])
 
 
 
-	return <SortContext.Provider value={{ data: filteredAndSortedCases, sortData }}>
-		<PaperContainer justify="center">
-			<TextField
-				variant="outlined"
-				label="Search..."
-				value={searchTerm}
-				style={{
-					marginBottom: 15
-				}}
-				onChange={handleSearchChange} />
+  return <SortContext.Provider value={{ data: filteredAndSortedCases, sortData }}>
+    <PaperContainer justify="center">
+      <TextField
+        variant="outlined"
+        label="Search..."
+        value={searchTerm}
+        style={{
+          marginBottom: 15
+        }}
+        onChange={handleSearchChange} />
 
-			<TableContainer component={Paper}>
-				<Table>
-					<WorldTableHead opts={options} />
-					{
-						loading ?
-							<TableLoadingRow />
-							: <WorldTableBody data={filteredAndSortedCases} />
-					}
-				</Table>
-			</TableContainer>
-		</PaperContainer>
-	</SortContext.Provider>
+      <TableContainer component={Paper}>
+        <Table>
+          <WorldTableHead opts={options} />
+          {
+            loading ?
+              <TableLoadingRow />
+              : <WorldTableBody data={filteredAndSortedCases} />
+          }
+        </Table>
+      </TableContainer>
+    </PaperContainer>
+  </SortContext.Provider>
 }
 
 

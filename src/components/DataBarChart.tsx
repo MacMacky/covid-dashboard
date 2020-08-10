34b71e8f2ae871113093687fde3f90fef298fae7
Mark Abeto <mac21macky@gmailcom>
2020-08-10 18:13:02 +0800

    Modify components logic

--- src/components/DataBarChart.tsx
+++ src/components/DataBarChart.tsx
@@ -8,29 +8,29 @@ import { chartColors } from './renderChart';
 
 
 const DataBarChart = ({ loading, data, color, dataKey, dataKeys }: ChartProps) => {
-	let bars = null, isMultiple = false
-	if (Array.isArray(dataKeys)) {
-		isMultiple = true
-		bars = dataKeys.map((key, i) => (
-			<Bar key={key} dataKey={key} fill={chartColors[i]} />
-		))
-	} else {
-		bars = <Bar dataKey={dataKey} fill={color} />
-	}
+  let bars = null, isMultiple = false
+  if (Array.isArray(dataKeys)) {
+    isMultiple = true
+    bars = dataKeys.map((key, i) => (
+      <Bar key={key} dataKey={key} fill={chartColors[i]} />
+    ))
+  } else {
+    bars = <Bar dataKey={dataKey} fill={color} />
+  }
 
-	return (
-		<ResponsiveContainer width={1000} height={250}>
-			{loading ? <Skeleton height={250} variant="rect" /> :
-				<BarChart data={data || []}>
-					<CartesianGrid strokeDasharray="3 3" />
-					<XAxis dataKey="date" />
-					<YAxis width={100} />
-					<Tooltip content={!isMultiple ? CountryToolTip : CountryToolTipTwo} />
-					<Legend verticalAlign="top" />
-					{bars}
-				</BarChart>
-			}
-		</ResponsiveContainer>)
+  return (
+    <ResponsiveContainer width={1000} height={250} >
+      {loading ? <Skeleton height={250} variant="rect" /> :
+        <BarChart data={data || []}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis width={100} />
+          <Tooltip content={!isMultiple ? CountryToolTip : CountryToolTipTwo} />
+          <Legend verticalAlign="top" />
+          {bars}
+        </BarChart>
+      }
+    </ResponsiveContainer>)
 }
 
 export default DataBarChart
--- src/components/LabelWithIcon.tsx
+++ src/components/LabelWithIcon.tsx
@@ -6,26 +6,26 @@ import { Grid, Typography } from '@material-ui/core'
 
 
 const LabelWithIcon = ({ Icon, iconSize = 60, color, label, value = 0, loading = false }:
-	{ Icon: React.ElementType, iconSize?: number, color: string, label: string, value?: number, loading?: boolean }) => {
-	return (
-		<Grid item md={4}>
-			<Grid container justify="center">
-				<Grid item>
-					<Icon style={{ fontSize: iconSize, fill: color }} />
-				</Grid>
-				<Grid item>
-					<Typography children={label} variant="h3" style={{ color }} />
-				</Grid>
-			</Grid>
-			<Grid justify="center" container>
-				<Grid item md={6}>
-					<Typography variant="h4" component="div" align="center">
-						{loading ? <Skeleton /> : value ? formatNumber(value) : 'Unknown'}
-					</Typography>
-				</Grid>
-			</Grid>
-		</Grid>
-	)
+  { Icon: React.ElementType, iconSize?: number, color: string, label: string, value?: number, loading?: boolean }) => {
+  return (
+    <Grid item md={4} xs={12} sm={12} lg={4}>
+      <Grid container justify="center">
+        <Grid item>
+          <Icon style={{ fontSize: iconSize, fill: color }} />
+        </Grid>
+        <Grid item>
+          <Typography children={label} variant="h3" style={{ color, marginLeft: 10 }} />
+        </Grid>
+      </Grid>
+      <Grid justify="center" container>
+        <Grid item md={6}>
+          <Typography variant="h4" component="div" align="center">
+            {loading ? <Skeleton /> : value ? formatNumber(value) : 'Unknown'}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default LabelWithIcon

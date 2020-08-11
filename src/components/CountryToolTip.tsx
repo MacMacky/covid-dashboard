c3ab78cc275f3c8a4df07494f99db54bcae23fa2
Mark Abeto <mac21macky@gmailcom>
2020-08-11 17:07:34 +0800

    Add default values if 'payload' is null

--- src/components/CountryToolTip.tsx
+++ src/components/CountryToolTip.tsx
@@ -5,8 +5,8 @@ import { Paper, Typography } from '@material-ui/core'
 
 const CountryToolTip = ({ payload, label, active }: { payload: any, label: string, active: boolean }) => {
   if (active) {
-    const { dataKey, value, color } = payload[0]
-    const { code, country } = payload[0].payload
+    const { dataKey = '', value = '', color = '', payload: p = {} } = payload ? payload[0] : {}
+    const { code, country } = p
     return (
       <Paper style={{ padding: 15, textAlign: 'center' }}>
         <Typography variant="h5" children={country} />

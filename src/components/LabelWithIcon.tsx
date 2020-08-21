9c518bcb604e3f69041a296a460cef8633efa768
Mark Abeto <mac21macky@gmailcom>
2020-08-21 17:17:18 +0800

    Modify component

--- src/components/LabelWithIcon.tsx
+++ src/components/LabelWithIcon.tsx
@@ -8,18 +8,18 @@ import { Grid, Typography } from '@material-ui/core'
 const LabelWithIcon = ({ Icon, iconSize = 60, color, label, value = 0, loading = false }:
   { Icon: React.ElementType, iconSize?: number, color: string, label: string, value?: number, loading?: boolean }) => {
   return (
-    <Grid item md={4} xs={12} sm={12} lg={4}>
+    <Grid item md={12} xs={12} sm={12} lg={4}>
       <Grid container justify="center">
         <Grid item>
           <Icon style={{ fontSize: iconSize, fill: color }} />
         </Grid>
         <Grid item>
-          <Typography children={label} variant="h3" style={{ color, marginLeft: 10 }} />
+          <Typography children={label} variant="h3" style={{ color, marginLeft: 10 }} gutterBottom />
         </Grid>
       </Grid>
       <Grid justify="center" container>
         <Grid item md={6}>
-          <Typography variant="h4" component="div" align="center">
+          <Typography variant="h4" component="div" align="center" gutterBottom>
             {loading ? <Skeleton /> : value ? formatNumber(value) : 'Unknown'}
           </Typography>
         </Grid>

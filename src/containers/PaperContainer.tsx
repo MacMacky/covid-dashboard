71dab7be4b4289cb59290472c2c4f01a2f263bf1
Mark Abeto <mac21macky@gmailcom>
2020-08-21 18:14:23 +0800

    Modify component logic

--- src/containers/PaperContainer.tsx
+++ src/containers/PaperContainer.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 import { makeStyles } from '@material-ui/styles'
 import { Grid, Paper, GridContentAlignment } from '@material-ui/core'
 
-const paperContainerStyles = makeStyles({
-	root: {
-		margin: 10,
-		width: 'auto',
-		height: 'auto',
-		padding: 25
-	}
+const paperContainerStyles = (height: string | number = 'auto') => makeStyles({
+  root: {
+    margin: 10,
+    width: 'auto',
+    height,
+    padding: 25
+  }
 })
 
-const PaperContainer = ({ children, justify = 'flex-start' }: { children: React.ReactNode, justify?: GridContentAlignment | any }) => {
-	const styles = paperContainerStyles()
-	return (
-		<Grid container component={Paper}
-			elevation={4}
-			justify={justify}
-			className={styles.root}
-			children={children}
-		/>
-	)
+const PaperContainer = ({ height = 'auto', children, justify = 'flex-start' }: { height?: number | string, children: React.ReactNode, justify?: GridContentAlignment | any }) => {
+  const styles = paperContainerStyles(height)()
+  return (
+    <Grid container component={Paper}
+      elevation={4}
+      justify={justify}
+      className={styles.root}
+      children={children}
+    />
+  )
 }
 
 export {
-	PaperContainer
+  PaperContainer
 }

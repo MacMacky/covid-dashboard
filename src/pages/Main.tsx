8308bdee9e7e0a2fec53ab044aa6df411f6fbae5
Mark Abeto <mac21macky@gmailcom>
2020-08-21 18:18:51 +0800

    Modify 'About' component structure

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,116 +1,51 @@
 import React from 'react';
-import theme from '../theme';
-import { Skeleton } from '@material-ui/lab'
-import { Autocomplete } from '@material-ui/lab'
+import LabelWithIcon from '../components/LabelWithIcon';
+import { RowData } from '../helpers/types';
+import { MAIN_API } from '../helpers/config';
 import { PaperContainer } from '../containers/PaperContainer';
-import { useFetch, useChangeDocumentTitle } from '../helpers/hooks';
-import { pick, log, getCountryData, groupByRegions } from '../helpers'
-import { ThemeProvider, Paper, Typography, makeStyles } from '@material-ui/core'
-import { PieChart, Cell, Pie, Tooltip, ResponsiveContainer } from 'recharts'
-
-
-
-const data = [
-  {
-    "name": "Group A",
-    "value": 2400,
-    another_one: 1
-  },
-  {
-    "name": "Group B",
-    "value": 4567,
-    another_one: 1
-  },
-  {
-    "name": "Group C",
-    "value": 1398,
-    another_one: 1
-  },
-  {
-    "name": "Group D",
-    "value": 9800,
-    another_one: 1
-  },
-  {
-    "name": "Group E",
-    "value": 3908,
-    another_one: 1
-  },
-  {
-    "name": "Group F",
-    "value": 4800,
-    another_one: 1
-  }
-];
-
-const toolTipStyles = makeStyles({
-  root: {
-    padding: 40
-  }
-})
-
-const CustomTooltip = (props: any) => {
-  if (props.active) {
-    const { name } = props.payload[0].payload.payload
-    return (
-      <Paper style={{ padding: '15px 25px', cursor: 'pointer' }}>
-        <Typography children={name} />
-      </Paper>
-    )
-  }
-}
+import { Grid, colors, Typography } from '@material-ui/core'
+import { useFetch, useChangeDocumentTitle, useToastCallback } from '../helpers/hooks';
+import { GroupAdd, PersonAddDisabled, PeopleAlt, Report, Healing, Language } from '@material-ui/icons'
 
 function Main() {
-  const [error, loading, response] = useFetch(['https://restcountries.eu/rest/v2/all',
-    'https://api.thevirustracker.com/free-api?countryTotals=ALL'])
-  const [pieChartData, setPieChartData] = React.useState<any[]>([])
+  const [error, loading, response] = useFetch<RowData>(MAIN_API, (response) => {
+    const [countries] = response || [[{}]]
+    return countries[0]
+  }, {})
+
   useChangeDocumentTitle()
-  // React.useEffect(() => {
-  //   if (!loading && response) {
-  //     let data = getCountryData(response[1].countryitems, ['ourid', 'title', 'code', 'total_cases'])
-  //     log(data.reduce((acc, o) => o.total_cases ? acc + o.total_cases : acc, 0))
-  //     data = data.map(o => {
-  //       if (o.total_cases) {
-  //         return { ...o, value: o.total_cases, name: o.title }
-  //       }
-  //     }).filter(o => o)
+  useToastCallback(error)
 
-
-  //     const dataByRegions = groupByRegions(response[0], data)
-  //     // log(dataByRegions)
-  //     setPieChartData(data)
-  //   }
-  // }, [loading])
   return (
-    <PaperContainer>
-
+    <PaperContainer height={875}>
+      <Grid container justify="center" style={{ marginBottom: 30 }}>
+        <Grid container justify="center">
+          <Grid item>
+            <Language style={{ fontSize: 60, color: colors.brown[500] }} />
+          </Grid>
+          <Grid item>
+            <Typography variant="h2" style={{ marginLeft: 10 }}>
+              Globe
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid container justify="center" >
+        <LabelWithIcon label="Total Recovered" loading={loading} value={response.recovered} color={colors.green[500]} Icon={Healing} />
+        <LabelWithIcon label="Total Cases" loading={loading} value={response.cases} color={colors.yellow[700]} Icon={PeopleAlt} />
+        <LabelWithIcon label="Total Deaths" loading={loading} value={response.deaths} color={colors.red[400]} Icon={PersonAddDisabled} />
+      </Grid>
+      <Grid container justify="center" >
+        <LabelWithIcon label="Active Cases" loading={loading} value={response.active} color={colors.orange[500]} Icon={GroupAdd} />
+        <LabelWithIcon label="Critical" loading={loading} value={response.critical} color={colors.red[500]} Icon={Report} />
+      </Grid>
+      <Grid container justify="center" >
+        <LabelWithIcon label="Today's Cases" loading={loading} value={response.todayCases} color={colors.orange[500]} Icon={GroupAdd} />
+        <LabelWithIcon label="Today's Deaths" loading={loading} value={response.deaths} color={colors.red[500]} Icon={Report} />
+      </Grid>
     </PaperContainer>
   );
 }
 
-
-
-// <PaperContainer>
-// {/* {
-//   !loading ? <ResponsiveContainer width="100%" height={3000}>
-//     <PieChart >
-//       <Pie data={pieChartData} dataKey="value" nameKey="name" cx="50%" cy="50%" fill="#82ca9d" label />
-
-//       <Tooltip content={CustomTooltip} />
-//     </PieChart>
-//   </ResponsiveContainer> : <Skeleton variant="circle" height={500} width={500} />
-
-// } */}
-// </PaperContainer>
-
-// <Autocomplete
-// id="combo-box-demo"
-// options={countries}
-// freeSolo
-// disableClearable
-// style={{ width: 350 }}
-// getOptionLabel={option => option.title}
-// renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
-// />
 
 export default Main;

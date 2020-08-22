b3bcab2b4c496dc7e0e89ff3d8717fbe9f871562
Mark Abeto <mac21macky@gmailcom>
2020-08-22 18:39:52 +0800

    Add new config for 'wiki'

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,2 +1,3 @@
 export const MAIN_API = 'https://coronavirus-19-api.herokuapp.com/countries'
 export const TIME_SERIES_API = 'https://pomber.github.io/covid19/timeseries.json'
+export const COVID_WIKI_API = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&origin=*&titles=Coronavirus%20disease%202019'

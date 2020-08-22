2a3222d94e40a5300dea3ecf0f1598f20d61073d
Mark Abeto <mac21macky@gmailcom>
2020-08-22 17:52:21 +0800

    Updates

--- precache-manifest.24b1b472ff156f267af6c17780cdb170.js
+++ precache-manifest.24b1b472ff156f267af6c17780cdb170.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "560f0f729d6a43303a99cbbfac5d3caf",
+    "revision": "be1ff045ef258f65e09ccccad8f478b4",
     "url": "/covid-dashboard/index.html"
   },
   {
-    "revision": "a477422c7ee3636e2f90",
+    "revision": "de3485efb1a3d90333f9",
     "url": "/covid-dashboard/static/css/main.a4dd9732.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid-dashboard/static/js/2.c3e658ab.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a477422c7ee3636e2f90",
-    "url": "/covid-dashboard/static/js/main.de5d0d4d.chunk.js"
+    "revision": "de3485efb1a3d90333f9",
+    "url": "/covid-dashboard/static/js/main.730c5c6a.chunk.js"
   },
   {
     "revision": "8900855fb643ca08d1bf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-dashboard/precache-manifest.c62bb282c42cc761caf946dd1d16408e.js"
+  "/covid-dashboard/precache-manifest.24b1b472ff156f267af6c17780cdb170.js"
 );
 
 self.addEventListener('message', (event) => {

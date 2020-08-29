f13fa232c7411c1561600628a7a2b1ea6bea5fad
Mark <mark@dnamicro.com>
2020-08-29 11:27:09 +0800

    Updates

--- precache-manifest.5aeb24302c8b96959826517bb7a4c8fd.js
+++ precache-manifest.5aeb24302c8b96959826517bb7a4c8fd.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a1b3c6ac9ba45c67bdde845f3e2c931e",
+    "revision": "60cc306283ef1453057b331124054bc3",
     "url": "/covid-dashboard/index.html"
   },
   {
-    "revision": "19957a03af170aaa51d8",
+    "revision": "48e454f134cb0546ef68",
     "url": "/covid-dashboard/static/css/main.8261f8f3.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid-dashboard/static/js/2.5e6b57f6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "19957a03af170aaa51d8",
-    "url": "/covid-dashboard/static/js/main.182c7ab2.chunk.js"
+    "revision": "48e454f134cb0546ef68",
+    "url": "/covid-dashboard/static/js/main.34402923.chunk.js"
   },
   {
     "revision": "8900855fb643ca08d1bf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-dashboard/precache-manifest.4820f2fd7f18f65b43565ad81c39d287.js"
+  "/covid-dashboard/precache-manifest.5aeb24302c8b96959826517bb7a4c8fd.js"
 );
 
 self.addEventListener('message', (event) => {
